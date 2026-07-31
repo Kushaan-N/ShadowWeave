1ef872cea17dae0abd921f9a67a037310379a0dd
Kushaan <kushaannaskar@gmail.com>
2026-07-30 18:40:06 -0700

    Export BEVProjector, bev_flow, and ShadowVisualizer

--- shadowweave/shadow/__init__.py
+++ shadowweave/shadow/__init__.py
@@ -1,11 +1,16 @@
+from .bev import BEVProjector, bev_flow
+from .occupancy import GeometricOccupancyField, OccupancyField
 from .raycast import ShadowRaycaster
-from .occupancy import OccupancyField, GeometricOccupancyField
-from .zones import pool_to_zones, pool_predictions_to_zones, pool_predictions_to_zones_torch
+from .visualizer import ShadowVisualizer
+from .zones import pool_predictions_to_zones, pool_predictions_to_zones_torch, pool_to_zones
 
 __all__ = [
     "ShadowRaycaster",
+    "BEVProjector",
+    "bev_flow",
     "OccupancyField",
     "GeometricOccupancyField",
+    "ShadowVisualizer",
     "pool_to_zones",
     "pool_predictions_to_zones",
     "pool_predictions_to_zones_torch",
