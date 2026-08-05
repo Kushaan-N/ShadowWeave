351df3fde0e46c449430b8ce1ca16646e0805b5a
Kushaan <kushaannaskar@gmail.com>
2026-08-05 08:54:43 -0700

    Export the world models from the package root
    Importers now use `from shadowweave.world_model import build_world_model` rather
    than reaching into a specific module file.

--- shadowweave/world_model/__init__.py
+++ shadowweave/world_model/__init__.py
@@ -1,3 +1,21 @@
-from .diffusion import WorldModel
+from .ddpm import DiffusionWorldModel
+from .unet import (
+    CondUNet,
+    ConvLSTMCell,
+    DoubleConv,
+    WorldModel,
+    WorldModelConvLSTM,
+    build_world_model,
+    n_input_channels,
+)
 
-__all__ = ["WorldModel"]
+__all__ = [
+    "build_world_model",
+    "WorldModel",
+    "WorldModelConvLSTM",
+    "DiffusionWorldModel",
+    "CondUNet",
+    "ConvLSTMCell",
+    "DoubleConv",
+    "n_input_channels",
+]
